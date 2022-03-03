c3a7bc9df4c5fd1941cc50aabb7799b325741fba
Nikolaj Geisle <niko737@edu.ucl.dk>
2022-03-03 11:11:37 +0100

    import builders from cypress project

---
+++ umbraco/builders/dataTypes/index.ts
@@ -0,0 +1,5 @@
+export { FormPickerDataTypeBuilder } from './formPickerDataTypeBuilder';
+export { LabelDataTypeBuilder } from './labelDataTypeBuilder';
+export { TextBoxDataTypeBuilder } from './textBoxDataTypeBuilder';
+export { DropDownDataTypeBuilder } from './dropDownDataTypeBuilder';
+export { GridDataTypeBuilder } from './gridDataTypeBuilder';
