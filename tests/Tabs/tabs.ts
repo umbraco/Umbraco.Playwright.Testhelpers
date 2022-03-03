da44ed911c84a072952337de01a581cb5dbd5b87
Nikolaj Geisle <niko737@edu.ucl.dk>
2022-03-03 12:54:55 +0100

    Add tabs

---
+++ tests/Tabs/tabs.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+import { DocumentTypeBuilder } from '../../umbraco/builders';
+import { AliasHelper } from '../../umbraco/helpers';
+
+test.describe('Login', () => {
+
+});
