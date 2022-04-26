322fc565294b24ee13413b39caaddba3c8c97e8a
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2022-04-26 16:27:50 +0200

    Fix getTreeItem

--- umbraco/helpers/UiHelpers.ts
+++ umbraco/helpers/UiHelpers.ts
@@ -52,15 +52,21 @@ export class UiHelpers {
       .locator('li > .umb-tree-root a[href*=' + treeName + ']')
       .locator("xpath=ancestor::li");
 
-    for(const index in itemNamePathArray){
+    for(let i = 0; i < itemNamePathArray.length; i++){
       // We want to find the outer li of the tree item, to be able to search deeper, there may be multiple results
       // for multiple levels of nesting, but it should be okay to pick last, since that should pick the immediate parent
       // the search goes from outer most to inner most (I think)
       finalLocator = await finalLocator
-        .locator('.umb-tree-item__label >> text=' + itemNamePathArray[index])
+        .locator('.umb-tree-item__label >> text=' + itemNamePathArray[i])
         .locator('xpath=ancestor::li[contains(@class, "umb-tree-item")]')
         .last();
 
+      // We don't want to click the expand option if we're on the last item, 
+      // since that can cause issues when clicking the "root" items, such as Document Types.
+      if(i+1 == itemNamePathArray.length){
+        break;
+      }
+      
       // Get the UL with the collapsed state, if it exists
       const ulObject = await finalLocator.locator(".collapsed");
       if(await ulObject.count() > 0){
