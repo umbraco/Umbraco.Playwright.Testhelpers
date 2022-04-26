4cea4c951801f7e5fcd69627d3885aa0adc8bca8
Nikolaj Geisle <niko737@edu.ucl.dk>
2022-04-26 08:24:07 +0200

    Update memberTypes.spec.ts

--- tests/Settings/memberTypes.spec.ts
+++ tests/Settings/memberTypes.spec.ts
@@ -1,6 +1,6 @@
 import {expect} from '@playwright/test';
-import {test} from '../../../umbraco/helpers';
-import {DocumentTypeBuilder} from "../../../umbraco/builders";
+import {test} from '../../umbraco/helpers';
+import {DocumentTypeBuilder} from "../../umbraco/builders";
 
 test.describe('Member Types', () => {
   test.beforeEach(async ({page, umbracoApi}) => {
