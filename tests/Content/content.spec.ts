ef2e305faa2424f6356997977e79d85f20836614
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-06-23 13:53:45 +0200

    Migrate copy content

--- tests/Content/content.spec.ts
+++ tests/Content/content.spec.ts
@@ -7,33 +7,77 @@ test.describe('Content tests', () => {
   test.beforeEach(async ({page, umbracoApi}) => {
     await umbracoApi.login();
   });
-
-  test('Create data type', async ({ page, umbracoApi, umbracoUi }) => {
-    const name = "Test";
-
-    await umbracoApi.documentTypes.ensureNameNotExists(name);
-    await umbracoApi.templates.ensureNameNotExists(name);
-
-    const tabsDocType = new DocumentTypeBuilder()
-      .withName(name)
-      .withAlias("aTest")
+  
+  test('Copy content', async ({ page, umbracoApi, umbracoUi }) => {
+    const rootDocTypeName = "Test document type";
+    const childDocTypeName = "Child test document type";
+    const firstRootNodeName = "1) Home";
+    const childNodeName = "1) Child";
+    const secondRootNodeName = "2) Home";
+    
+    const childDocType = new DocumentTypeBuilder()
+      .withName(childDocTypeName)
+      .build()
+    
+    await umbracoApi.content.deleteAllContent();
+    await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
+    await umbracoApi.documentTypes.ensureNameNotExists(childDocTypeName);
+    
+    const createdChildDocType = await umbracoApi.documentTypes.save(childDocType);
+    
+    const rootDocType = new DocumentTypeBuilder()
+      .withName(rootDocTypeName)
       .withAllowAsRoot(true)
-      .withDefaultTemplate(name)
+      .withAllowedContentTypes(createdChildDocType.id)
       .build();
-
-    const response = await umbracoApi.documentTypes.save(tabsDocType);
-
-    const content = new ContentBuilder()
-      .withContentTypeAlias(response.alias)
+    
+    const createdRootDocType = await umbracoApi.documentTypes.save(rootDocType);
+    
+    // TODO: Make some constants for actions.
+    const rootContentNode = new ContentBuilder()
+      .withContentTypeAlias(createdRootDocType.alias)
       .withAction("saveNew")
       .addVariant()
-        .withName("SomeContent")
+        .withName(firstRootNodeName)
+        .withSave(true)  // We should probably just default to true...
+      .done()
+      .build();
+
+    const savedRootNode = await umbracoApi.content.save(rootContentNode);
+
+    const secondRootNode = new ContentBuilder()
+      .withContentTypeAlias(createdRootDocType.alias)
+      .withAction("saveNew")
+      .addVariant()
+      .withName(secondRootNodeName)
+      .withSave(true)
+      .done()
+      .build();
+
+    await umbracoApi.content.save(secondRootNode);
+    
+    const childContentNode = new ContentBuilder()
+      .withContentTypeAlias(createdChildDocType.alias)
+      .withAction("saveNew")
+      .withParent(savedRootNode.id)
+      .addVariant()
+        .withName(childNodeName)
         .withSave(true)
       .done()
       .build();
     
-    await umbracoApi.content.save(content);
-    await page.pause();
+    await umbracoApi.content.save(childContentNode);
+    
+    await umbracoUi.refreshContentTree();
+    
+    await umbracoUi.clickElement(umbracoUi.getTreeItem("content", [firstRootNodeName, childNodeName]), {button: "right", force: true})
+    await umbracoUi.clickElement(umbracoUi.getContextMenuAction("action-copy"))
+    await page.locator('.umb-pane [data-element="tree-item-' + secondRootNodeName + '"]').click();
+    await page.locator('.umb-dialog-footer > .btn-primary').click();
+    await expect(page.locator('.alert-success')).toBeVisible();
+
+    await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
+    await umbracoApi.documentTypes.ensureNameNotExists(childDocTypeName);
   });
 });
 
