0c32f2f487ca59f16287878a2f7d0e7611a6daac
Zeegaan <nge@umbraco.dk>
2022-06-24 13:59:22 +0200

    Add package test

--- tests/Packages/packages.spec.ts
+++ tests/Packages/packages.spec.ts
@@ -29,68 +29,68 @@ test.describe('Packages', () => {
       .withAllowAsRoot(true)
       .build();
 
-    umbracoApi.documentTypes.saveDocumentType(rootDocType).then((generatedRootDocType) => {
-      const rootDocTypeAlias = generatedRootDocType["alias"];
+    const generatedRootDocType = await umbracoApi.documentTypes.saveDocumentType(rootDocType);
+    
+    const rootDocTypeAlias = generatedRootDocType["alias"];
 
-      const rootContentNode = new ContentBuilder()
-        .withContentTypeAlias(rootDocTypeAlias)
-        .withAction("saveNew")
-        .addVariant()
-        .withName(nodeName)
-        .withSave(true)
-        .done()
-        .build();
-      umbracoApi.content.save(rootContentNode).then((generatedContent) => {
-        CreatePackage(umbracoApi, generatedContent.Id);
-      });
-    });
+    const rootContentNode = new ContentBuilder()
+      .withContentTypeAlias(rootDocTypeAlias)
+      .withAction("saveNew")
+      .addVariant()
+      .withName(nodeName)
+      .withSave(true)
+      .done()
+      .build();
+      const generatedContent = await umbracoApi.content.save(rootContentNode);
+      await CreatePackage(umbracoApi, generatedContent.Id);
+      
   }
 
-  // test('Creates a simple package', async ({page, umbracoApi, umbracoUi}) => {
-  //   await umbracoApi.packages.ensureNameNotExists(packageName);
-  //   await umbracoApi.content.deleteAllContent();
-  //   await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
-  //
-  //   const rootDocType = new DocumentTypeBuilder()
-  //     .withName(rootDocTypeName)
-  //     .withAllowAsRoot(true)
-  //     .build();
-  //
-  //   const generatedRootDocType = await umbracoApi.documentTypes.saveDocumentType(rootDocType);
-  //   const rootDocTypeAlias = generatedRootDocType["alias"];
-  //
-  //   const rootContentNode = new ContentBuilder()
-  //     .withContentTypeAlias(rootDocTypeAlias)
-  //     .withAction("saveNew")
-  //     .addVariant()
-  //     .withName(nodeName)
-  //     .withSave(true)
-  //     .done()
-  //     .build();
-  //
-  //   await umbracoApi.saveContent(rootContentNode);
-  //   await page.pause();
-  //
-  //   // Navigate to create package section
-  //   await umbracoUi.goToSection('packages');
-  //   await page.locator("button", {hasText: "Created"}).click();
-  //   await page.locator("button", {hasText: "Create package"}).click();
-  //
-  //   // Fill out package creation form
-  //   await page.waitForTimeout(1000);
-  //   await page.locator("#headerName").type(packageName);
-  //   await page.locator('.controls > .umb-node-preview-add').click();
-  //   await page.locator('.umb-tree-item__label').first().click();
-  //   await page.locator("button", {hasText: "Create"}).click();
-  //
-  //   // Navigate pack to packages and Assert the file is created
-  //   await umbracoUi.goToSection('packages');
-  //   await page.locator("button", {hasText: "Created"}).click();
-  //   await expect(await page.locator("body", {hasText: packageName}));
-  //
-  //   // Cleanup
-  //   await umbracoApi.packages.ensureNameNotExists(packageName);
-  //   await umbracoApi.content.deleteAllContent();
-  //   await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
-  // });
+  test('Creates a simple package', async ({page, umbracoApi, umbracoUi}) => {
+    await umbracoApi.packages.ensureNameNotExists(packageName);
+    await umbracoApi.content.deleteAllContent();
+    await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
+
+    const rootDocType = new DocumentTypeBuilder()
+      .withName(rootDocTypeName)
+      .withAllowAsRoot(true)
+      .build();
+
+    const generatedRootDocType = await umbracoApi.documentTypes.save(rootDocType);
+    const rootDocTypeAlias = generatedRootDocType["alias"];
+
+    const rootContentNode = new ContentBuilder()
+      .withContentTypeAlias(rootDocTypeAlias)
+      .withAction("saveNew")
+      .addVariant()
+      .withName(nodeName)
+      .withSave(true)
+      .done()
+      .build();
+
+    await umbracoApi.content.save(rootContentNode);
+    await page.pause();
+
+    // Navigate to create package section
+    await umbracoUi.goToSection('packages');
+    await page.locator("button", {hasText: "Created"}).click();
+    await page.locator("button", {hasText: "Create package"}).click();
+
+    // Fill out package creation form
+    await page.waitForTimeout(1000);
+    await page.locator("#headerName").type(packageName);
+    await page.locator('.controls > .umb-node-preview-add').click();
+    await page.locator('.umb-tree-item__label').first().click();
+    await page.locator("button", {hasText: "Create"}).click();
+
+    // Navigate pack to packages and Assert the file is created
+    await umbracoUi.goToSection('packages');
+    await page.locator("button", {hasText: "Created"}).click();
+    await expect(await page.locator("body", {hasText: packageName})).toBeVisible();
+
+    // Cleanup
+    await umbracoApi.packages.ensureNameNotExists(packageName);
+    await umbracoApi.content.deleteAllContent();
+    await umbracoApi.documentTypes.ensureNameNotExists(rootDocTypeName);
+  });
 });
