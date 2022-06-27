234626660a8010cc0f0097ab533aa946d62c4d18
Zeegaan <nge@umbraco.dk>
2022-06-27 15:24:59 +0200

    Add membergroups test

---
+++ tests/Members/memberGroups.spec.ts
@@ -0,0 +1,26 @@
+import {expect} from '@playwright/test';
+import {ConstantHelper, test} from '../../../umbraco/helpers';
+
+test.describe('Packages', () => {
+
+  test.beforeEach(async ({page, umbracoApi}) => {
+    await umbracoApi.login();
+  });
+
+  test('Create member group', async ({page, umbracoApi, umbracoUi}) => {
+    const name = "Test Group";
+    await umbracoApi.memberGroups.ensureNameNotExists(name);
+    await umbracoUi.goToSection('member');
+
+    await umbracoUi.clickElement(umbracoUi.getTreeItem("member", ["Member Groups"]), { button: "right"});
+    await umbracoUi.clickElement(umbracoUi.getContextMenuAction(ConstantHelper.actions.create));
+    await umbracoUi.setEditorHeaderName(name)
+    await umbracoUi.clickElement(umbracoUi.getButtonByLabelKey(ConstantHelper.buttons.save));
+    
+    // Assert
+    await umbracoUi.isSuccessNotificationVisible();
+    
+    // Clean up
+    await umbracoApi.memberGroups.ensureNameNotExists(name);
+  });
+});
