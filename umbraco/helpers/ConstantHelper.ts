c5dcffa6507ac343d38b164679581140bd312abb
Zeegaan <nge@umbraco.dk>
2022-06-27 15:29:09 +0200

    Add member constant

--- umbraco/helpers/ConstantHelper.ts
+++ umbraco/helpers/ConstantHelper.ts
@@ -1,22 +1,23 @@
 ﻿export class ConstantHelper {
     public static readonly buttons = {
-        save: "buttons_save",
-        delete: "general_delete",
         close: "general_close",
-        saveAndPublish: "buttons_saveAndPublish"
+        delete: "general_delete",
+        save: "buttons_save",
+        saveAndPublish: "buttons_saveAndPublish",
     }
 
     public static readonly sections = {
+        content: "content",
+        member: "member",
         settings: "settings",
-        content: "content"
     }
 
     public static readonly actions = {
+        copy: "action-copy",
         create: "action-create",
         delete: "action-delete",
-        sort: "action-sort",
         move: "action-move",
-        copy: "action-copy"
+        sort: "action-sort",
     }
 }
 
