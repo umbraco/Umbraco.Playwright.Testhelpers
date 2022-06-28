004b564482aae9633bb42dfdccf17733db0ad22b
Zeegaan <nge@umbraco.dk>
2022-06-28 10:14:17 +0200

    Add editTemplate method to Templates Api helper

--- umbraco/helpers/TemplatesApiHelper.ts
+++ umbraco/helpers/TemplatesApiHelper.ts
@@ -8,6 +8,26 @@ export class TemplatesApiHelper{
     this.api = api;
   }
   
+  async editTemplate(name, content){
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/backoffice/UmbracoTrees/TemplatesTree/GetNodes?id=-1');
+    const searchBody = await JsonHelper.getBody(response);
+
+    let templateId = null;
+
+    for (const sb of searchBody) {
+      if (sb.name == name) {
+        templateId = sb.id;
+      }
+    }
+
+    if (templateId !== null) {
+      const templateResponse = await this.api.get(this.api.baseUrl + '/umbraco/backoffice/UmbracoApi/Template/GetById?id=' + templateId);
+      const template = await JsonHelper.getBody(templateResponse);
+      template.content = content;
+      return await this.saveTemplate(template);
+    }
+  }
+  
   async ensureNameNotExists(name: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/backoffice/UmbracoTrees/TemplatesTree/GetNodes?id=-1');
     const searchBody = await JsonHelper.getBody(response);
