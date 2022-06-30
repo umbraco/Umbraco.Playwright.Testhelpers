1b07080d977f60762546837bbe5c9f39c2b91c5f
Zeegaan <nge@umbraco.dk>
2022-06-30 13:03:06 +0200

    Add contentApps constants

--- umbraco/helpers/ConstantHelper.ts
+++ umbraco/helpers/ConstantHelper.ts
@@ -1,4 +1,15 @@
 ﻿export class ConstantHelper {
+
+    public static readonly actions = {
+        copy: "action-copy",
+        create: "action-create",
+        dataType: "action-data-type",
+        documentType: "action-documentType",
+        delete: "action-delete",
+        move: "action-move",
+        sort: "action-sort",
+    }
+    
     public static readonly buttons = {
         close: "general_close",
         delete: "general_delete",
@@ -9,22 +20,16 @@
         submit: "general_submit",
         rollback: "actions_rollback"
     }
+    
+    public static readonly contentApps = {
+        info: '[data-element="sub-view-umbInfo"]',
+    }
 
     public static readonly sections = {
         content: "content",
         member: "member",
         packages: "packages",
         settings: "settings",
-    }
-
-    public static readonly actions = {
-        copy: "action-copy",
-        create: "action-create",
-        dataType: "action-data-type",
-        documentType: "action-documentType",
-        delete: "action-delete",
-        move: "action-move",
-        sort: "action-sort",
     }
 }
 
