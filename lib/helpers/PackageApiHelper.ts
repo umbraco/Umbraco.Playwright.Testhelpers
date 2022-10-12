226b7368577be521031c21e55282be590059b5d6
Andreas Zerbst <andr317c@live.dk>
2022-10-12 13:35:38 +0200

    Added a helper for checking if a specific package exist

--- lib/helpers/PackageApiHelper.ts
+++ lib/helpers/PackageApiHelper.ts
@@ -1,8 +1,8 @@
-import { ApiHelpers } from "./ApiHelpers";
-import { JsonHelper } from "./JsonHelper";
+import {ApiHelpers} from "./ApiHelpers";
+import {JsonHelper} from "./JsonHelper";
 import {Package} from "@umbraco/json-models-builders";
 
-export class PackageApiHelper{
+export class PackageApiHelper {
   api: ApiHelpers
 
   constructor(api: ApiHelpers) {
@@ -26,7 +26,26 @@ export class PackageApiHelper{
     }
   }
 
-  async save(packageToSave: Package){
+  async doesNameExist(name: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/backoffice/umbracoapi/package/GetCreatedPackages');
+    const searchBody = await JsonHelper.getBody(response);
+
+    let packageId = null;
+    let doesExist = false;
+    
+    for (const sb of searchBody) {
+      if (sb.name == name) {
+        packageId = sb.id;
+      }
+    }
+
+    if (packageId !== null) {
+      doesExist = true;
+    }
+    return doesExist
+  }
+
+  async save(packageToSave: Package) {
     await this.api.post(this.api.baseUrl + '/umbraco/backoffice/umbracoapi/package/PostSavePackage', packageToSave);
   }
 }
