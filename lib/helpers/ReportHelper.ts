4769f8046d79075ccb88e41095fd254028c8b59e
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-11-10 19:02:39 +0100

    Dont check for retry 0, as that has not failed

--- lib/helpers/ReportHelper.ts
+++ lib/helpers/ReportHelper.ts
@@ -12,7 +12,7 @@ export class ReportHelper {
     console.log("Trying to report......")
     console.log("Retry: " + testInfo.retry)
     console.log("Is ci: " + process.env.CI)
-    if(testInfo.retry && testInfo.retry === 0 && process.env.CI){
+    if(testInfo.retry > 0 && process.env.CI){
       await this.api.post("https://functionapp-221110123128.azurewebsites.net/api/PlaywrightTableData", { "TestName": testInfo.title, "CommitId": process.env.CommitId, "RetryNumber": testInfo.retry});
     }
   }
