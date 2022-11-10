b8357757a35138101e36215bc5d995362a4cd33a
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-11-10 13:26:02 +0100

    Feature/add report helper (#21)
    * Add ReportHelper.ts
    
    * Remove unused test-folder
    
    * Add guard logic
    
    * Update lib/helpers/ReportHelper.ts
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    
    Co-authored-by: Zeegaan <nge@umbraco.dk>
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>

---
+++ lib/helpers/ReportHelper.ts
@@ -0,0 +1,16 @@
+﻿import {ApiHelpers} from "./ApiHelpers";
+import {TestInfo} from "@playwright/test";
+
+export class ReportHelper {
+  api: ApiHelpers
+
+  constructor(api: ApiHelpers) {
+    this.api = api;
+  }
+
+  async report(testInfo: TestInfo) {
+    if(testInfo.retry && testInfo.retry === 0 && process.env.CI){
+      await this.api.post("https://functionapp-221110123128.azurewebsites.net/api/PlaywrightTableData", { "TestName": testInfo.title, "CommitId": process.env.CommitId});
+    }
+  }
+}
