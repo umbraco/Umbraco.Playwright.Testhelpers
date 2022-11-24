fd09a995c204a26a8932a526a093812f441b4f6d
Andreas Zerbst <andr317c@live.dk>
2022-11-24 13:54:14 +0100

    Added an exists method for the datatypes so I can check if a datatype actually exists

--- lib/helpers/DatatypeApiHelper.ts
+++ lib/helpers/DatatypeApiHelper.ts
@@ -1,7 +1,6 @@
 import {ApiHelpers} from "./ApiHelpers";
 import {umbracoConfig} from "../../umbraco.config";
 import {JsonHelper} from "./JsonHelper";
-
 export class DatatypeApiHelper {
   api: ApiHelpers
   
@@ -29,5 +28,18 @@ export class DatatypeApiHelper {
     
     const datatype = await this.api.post(`${umbracoConfig.environment.baseUrl}/umbraco/backoffice/UmbracoApi/DataType/PostSave`, dataType);
     return await JsonHelper.getBody(datatype);
+  }
+
+  async exists(name){
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/backoffice/umbracotrees/datatypetree/GetNodes?id=-1');
+    const searchBody = await JsonHelper.getBody(response);
+    if (searchBody.length > 0) {
+      for (const sb of searchBody) {
+        if (sb.name === name) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
