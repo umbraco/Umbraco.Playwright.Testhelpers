8da51a3e36960df1eaf1bfe5c594a4a987135951
Andreas Zerbst <andr317c@live.dk>
2023-06-28 14:32:39 +0200

    Updated naming for Template and added exists functions, our create functions were also updated so we get the id of the newly created object returned.

--- lib/helpers/TemplateApiHelper.ts
+++ lib/helpers/TemplateApiHelper.ts
@@ -7,7 +7,7 @@ export class TemplateApiHelper {
     this.api = api;
   }
 
-  async ensureTemplateNameNotExistsAtRoot(name: string) {
+  async ensureNameNotExistsAtRoot(name: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/root?skip=0&take=10000');
     const json = await response.json();
 
@@ -21,7 +21,7 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async getTemplateById(id: string) {
+  async get(id: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/template/' + id);
     const json = await response.json();
 
@@ -31,7 +31,12 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async getTemplateByNameAtRoot(name: string) {
+  async exists(path: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/template/' + path);
+    return response.status() === 200;
+  }
+
+  async getByNameAtRoot(name: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/root?skip=0&take=10000');
     const json = await response.json();
 
@@ -46,20 +51,22 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async createTemplate(name: string, alias: string, content: string) {
+  async create(name: string, alias: string, content: string) {
     const templateData = {
       "name": name,
       "alias": alias,
       "content": content
     };
-    return await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/template', templateData);
+    const response = await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/template', templateData);
+    // Returns the id of the created template
+    return response.headers().location.split("/").pop();
   }
 
-  async deleteTemplateById(id: string) {
-    return await this.api.delete(this.api.baseUrl + '/umbraco/management/api/v1/dictionary/' + id);
+  async delete(id: string) {
+    return await this.api.delete(this.api.baseUrl + '/umbraco/management/api/v1/template/' + id);
   }
 
-  async deleteTemplateByNameAtRoot(name: string) {
+  async deleteByNameAtRoot(name: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/root?skip=0&take=10000');
     const json = await response.json();
 
@@ -73,11 +80,11 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async updateTemplateById(id: string, template: object) {
+  async update(id: string, template: object) {
     return await this.api.put(this.api.baseUrl + '/umbraco/management/api/v1/template/' + id, template);
   }
 
-  async getChildrenTemplatesById(id: string) {
+  async getChildren(id: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/children?skip=0&take=10000' + id);
     const json = await response.json();
 
@@ -87,7 +94,7 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async getTemplateItemsByIds(ids: string[]) {
+  async getItems(ids: string[]) {
     let idArray = 'id=' + ids[0];
     let i: number;
 
@@ -104,11 +111,11 @@ export class TemplateApiHelper {
     return null;
   }
 
-  async getTemplatesAtRoot() {
+  async getAllAtRoot() {
     return await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/root?skip=0&take=10000');
   }
 
-  async doesTemplateWithNameExistAtRoot(name: string) {
+  async nameExistsAtRoot(name: string) {
     const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/template/root?skip=0&take=10000');
     const json = await response.json();
 
