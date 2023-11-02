45fee7f236fa84947761ddae312f243eea7e1f9f
Andreas Zerbst <andr317c@live.dk>
2023-11-02 12:53:38 +0100

    Created constants for sections

---
+++ lib/helpers/ConstantHelper.ts
@@ -0,0 +1,12 @@
+﻿export class ConstantHelper {
+
+    public static readonly sections = {
+        content: "Content",
+        media: "Media",
+        settings: "Settings",
+        packages: "Packages",
+        members: "Members",
+        dictionary: "Dictionary",
+        users: "Users"
+    }
+}
