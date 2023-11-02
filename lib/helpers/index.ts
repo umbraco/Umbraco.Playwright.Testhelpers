8f81b7f0f058482b99e78cffb9726b4119a69898
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-11-02 13:19:31 +0100

    V2 Updated LogViewerHelper (#33)
    * Bumped version
    
    * Created constants for sections
    
    * Updated the index file to contain constantHelpers
    
    * Updated goToSection to check if all sections are visible before trying to click. Tidied up a bit of indentation

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -2,3 +2,4 @@ export { ApiHelpers } from './ApiHelpers';
 export { UiHelpers } from './UiHelpers';
 export { AliasHelper} from './AliasHelper';
 export { test } from './testExtension';
+export {ConstantHelper} from './ConstantHelper';
