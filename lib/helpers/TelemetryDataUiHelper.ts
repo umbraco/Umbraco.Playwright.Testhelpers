f624f5836a06c0930eb289667a85f81204442b8b
Nhu Dinh <hnd@umbraco.dk>
2023-12-26 19:26:43 +0700

    Added ui helper for TelemetryData

---
+++ lib/helpers/TelemetryDataUiHelper.ts
@@ -0,0 +1,25 @@
+import {Page, Locator, expect} from "@playwright/test";
+import {UiBaseLocators} from "./UiBaseLocators";
+
+export class TelemetryDataUiHelper extends UiBaseLocators {
+  private readonly telemetryDataTab: Locator;
+  private readonly telemetryDataLevelSlider: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.telemetryDataTab = page.getByRole('tab', {name: 'Telemetry Data'});
+    this.telemetryDataLevelSlider = page.locator('[name="telemetryLevel"] >> input[id=input]');
+  }
+
+  async clickTelemetryDataTab() {
+    await this.telemetryDataTab.click();
+  }
+
+  async changeTelemetryDataLevelValue(value: string) {
+    await this.telemetryDataLevelSlider.fill(value);
+  }
+
+  async doesTelemetryDataLevelHasValue(value: string) {
+    return await expect(this.telemetryDataLevelSlider).toHaveValue(value);
+  }
+}
