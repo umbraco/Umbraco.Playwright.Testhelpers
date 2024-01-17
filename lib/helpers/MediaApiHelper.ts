9781ed9e3aca5f343562dcce7cc9df73649a6312
Andreas Zerbst <andr317c@live.dk>
2024-01-17 19:32:07 +0100

    Added MediaApiHelper

---
+++ lib/helpers/MediaApiHelper.ts
@@ -0,0 +1,28 @@
+﻿import {ApiHelpers} from "./ApiHelpers";
+import {MediaBuilder} from "@umbraco/json-models-builders";
+
+export class MediaApiHelper {
+  api: ApiHelpers
+
+  constructor(api: ApiHelpers) {
+    this.api = api;
+  }
+
+  async create(media) {
+    if (media == null) {
+      return;
+    }
+    const response = await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/media', media)
+    return response.headers().location.split("/").pop();
+  }
+
+  async createDefaultMedia(mediaName: string, mediaTypeId: string) {
+    const media = new MediaBuilder()
+      .withContentTypeId(mediaTypeId)
+      .addVariant()
+      .withName(mediaName)
+      .done()
+      .build();
+    return await this.create(media);
+  }
+}
