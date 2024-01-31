069e4acc699db7a0a86bc0d34399c2b8285a565d
Nhu Dinh <hnd@umbraco.dk>
2024-01-31 14:19:41 +0700

    Fixed comments

--- lib/helpers/RelationTypeUiHelper.ts
+++ lib/helpers/RelationTypeUiHelper.ts
@@ -3,16 +3,20 @@ import {UiBaseLocators} from "./UiBaseLocators";
 
 export class RelationTypeUiHelper extends UiBaseLocators{
   private readonly relationTypeNameTxt: Locator;
-  private readonly parentToChildRadio: Locator;
-  private readonly bidirectionalRadio: Locator;
+  private readonly parentToChildRadioBtn: Locator;
+  private readonly bidirectionalRadioBtn: Locator;
   private readonly isDependencySlider: Locator;
+  private readonly parentDropDownBox: Locator;
+  private readonly childDropDownBox: Locator;
 
   constructor(page: Page) {
     super(page);
     this.relationTypeNameTxt = page.locator('#name #input');
-    this.parentToChildRadio = page.locator("uui-radio[label='Parent to child'] #button");
-    this.bidirectionalRadio = page.locator("uui-radio[label='Bidirectional'] #button");
+    this.parentToChildRadioBtn = page.locator("uui-radio[label='Parent to child'] #button");
+    this.bidirectionalRadioBtn = page.locator("uui-radio[label='Bidirectional'] #button");
     this.isDependencySlider = page.locator("umb-property-layout[label='Is dependency'] #slider");
+    this.parentDropDownBox = page.locator("umb-property-layout[label='Parent'] #native");
+    this.childDropDownBox = page.locator("umb-property-layout[label='Child'] #native");
   }
 
   async clickActionsMenuForRelationType(name: string) {
@@ -37,12 +41,12 @@ export class RelationTypeUiHelper extends UiBaseLocators{
     await this.relationTypeNameTxt.fill(name);
   }
 
-  async clickParentToChildRadio() {
-    await this.parentToChildRadio.click();
+  async clickParentToChildRadioButton() {
+    await this.parentToChildRadioBtn.click();
   }
 
-  async clickBidirectionalRadio() {
-    await this.bidirectionalRadio.click();
+  async clickBidirectionalRadioButton() {
+    await this.bidirectionalRadioBtn.click();
   }
 
   async clickIsDependencySlider() {
@@ -50,10 +54,10 @@ export class RelationTypeUiHelper extends UiBaseLocators{
   }
 
   async selectParentOption(option: string) {
-    await this.page.locator("umb-property-layout[label='Parent'] #native").selectOption({ label: option });
+    await this.parentDropDownBox.selectOption({ label: option });
   }
 
   async selectChildOption(option: string) {
-    await this.page.locator("umb-property-layout[label='Child'] #native").selectOption({ label: option });
+    await this.childDropDownBox.selectOption({ label: option });
   }
 }
