3f506cddf2ec3c9bce633c0a4a83c8ab1002e440
Nhu Dinh <hnd@umbraco.dk>
2024-02-28 15:30:05 +0700

    Added api helper for Document

--- lib/helpers/DocumentApiHelper.ts
+++ lib/helpers/DocumentApiHelper.ts
@@ -8,17 +8,124 @@ export class DocumentApiHelper {
     this.api = api;
   }
 
+  async get(id: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/document/' + id);
+    return await response.json();
+  }
+
+  async doesExist(id: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/document/' + id);
+    return response.status() === 200;
+  }
+
   async create(document) {
     if (document == null) {
       return;
     }
     const response = await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/document', document)
-    return response.headers().location.split("/").pop();
+    return response.headers().location.split("v1/document/").pop();
+  }
+
+  async delete(id: string) {
+    if (id == null) {
+      return;
+    }
+    const response = await this.api.delete(this.api.baseUrl + '/umbraco/management/api/v1/document/' + id);
+    return response.status();
+  }
+
+  async update(id: string, document) {
+    if (document == null) {
+      return;
+    }
+    return await this.api.put(this.api.baseUrl + '/umbraco/management/api/v1/document' + id, document)
+  }
+
+  async getAllAtRoot() {
+    return await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/tree/document/root?skip=0&take=10000');
+  }
+
+  async getChildren(id: string) {
+    const response = await this.api.get(`${this.api.baseUrl}/umbraco/management/api/v1/tree/document/children?parentId=${id}&skip=0&take=10000`);
+    const items = await response.json();
+    return items.items;
+  }
+
+  async doesNameExist(name: string) {
+    return await this.getByName(name);
+  }
+
+  private async recurseDeleteChildren(id: string) {
+    const items = await this.getChildren(id);
+
+    for (const child of items) {
+      if (child.hasChildren) {
+        await this.recurseDeleteChildren(child.id);
+      } else {
+        await this.delete(child.id);
+      }
+    }
+    return await this.delete(id);
+  }
+
+  private async recurseChildren(name: string, id: string, toDelete: boolean) {
+    const items = await this.getChildren(id);
+
+    for (const child of items) {
+      if (child.variants[0].name=== name) {
+        if (!toDelete) {
+          return await this.get(child.id);
+        }
+        if (child.hasChildren) {
+          return await this.recurseDeleteChildren(child.id);
+        } else {
+          return await this.delete(child.id);
+        }
+      } else if (child.hasChildren) {
+        await this.recurseChildren(name, child.id, toDelete);
+      }
+    }
+    return false;
+  }
+
+  async getByName(name: string) {
+    const rootDocuments = await this.getAllAtRoot();
+    const jsonDocuments = await rootDocuments.json();
+
+    for (const document of jsonDocuments.items) {
+      if (document.variants[0].name === name) {
+        return this.get(document.id);
+      } else if (document.hasChildren) {
+        const result = await this.recurseChildren(name, document.id, false);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return false;
+  }
+
+  async ensureNameNotExists(name: string) {
+    const rootDocuments = await this.getAllAtRoot();
+    const jsonDocuments = await rootDocuments.json();
+
+    for (const document of jsonDocuments.items) {
+      if (document.variants[0].name === name) {
+        if (document.hasChildren) {
+          await this.recurseDeleteChildren(document.id);
+        }
+        await this.delete(document.id);
+      } else {
+        if (document.hasChildren) {
+          await this.recurseChildren(name, document.id, true);
+        }
+      }
+    }
   }
 
   async createDefaultDocument(documentName: string, documentTypeId: string) {
     const document = new DocumentBuilder()
-      .withContentTypeId(documentTypeId)
+      .withDocumentTypeId(documentTypeId)
       .addVariant()
         .withName(documentName)
       .done()
