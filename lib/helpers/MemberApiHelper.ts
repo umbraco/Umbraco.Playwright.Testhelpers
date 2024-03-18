663e7a3ac465b0b2a1bc169930bd8a1d785c479c
Nhu Dinh <hnd@umbraco.dk>
2024-03-18 14:35:47 +0700

    Added ui and api helper for Member

---
+++ lib/helpers/MemberApiHelper.ts
@@ -0,0 +1,99 @@
+﻿import {ApiHelpers} from "./ApiHelpers";
+import {MemberBuilder} from "@umbraco/json-models-builders";
+
+export class MemberApiHelper {
+  api: ApiHelpers
+
+  constructor(api: ApiHelpers) {
+    this.api = api;
+  }
+
+  async get(id: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/member/' + id);
+    return await response.json();
+  }
+
+  async create(member) {
+    if (member == null) {
+      return;
+    }
+    const response = await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/member', member);
+    return response.headers().location.split("v1/member/").pop();
+  }
+
+  async delete(id: string) {
+    if (id == null) {
+      return;
+    }
+    const response = await this.api.delete(this.api.baseUrl + '/umbraco/management/api/v1/member/' + id);
+    return response.status();
+  }
+
+  async filterAll() {
+    return await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/filter/member?orderBy=username&skip=0&take=10000');
+  }
+
+  async filterByMemberTypeId(memberTypeId: string) {
+    return await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/filter/member?memberTypeId='+ memberTypeId + '&orderBy=username&skip=0&take=100');
+  }
+
+  async doesExist(id: string) {
+    const response = await this.api.get(this.api.baseUrl + '/umbraco/management/api/v1/member/' + id);
+    return response.status() === 200;
+  }
+
+  async doesNameExist(name: string) {
+    return await this.getByName(name);
+  }
+
+  async getByName(name: string) {
+    const rootMembers = await this.filterAll();
+    const jsonMembers = await rootMembers.json();
+
+    for (const member of jsonMembers.items) {
+      if (member.variants[0].name === name) {
+        return this.get(member.id);
+      }
+    }
+    return false;
+  }
+
+  async ensureNameNotExists(name: string) {
+    const rootMembers = await this.filterAll();
+    const jsonMembers = await rootMembers.json();
+
+    for (const member of jsonMembers.items) {
+      if (member.variants[0].name === name) {
+        return this.delete(member.id);
+      }
+    }
+    return null;
+  }
+
+  async createDefaultMember(memberName: string, memberTypeId: string, email: string, username: string, password: string) {
+    const member = new MemberBuilder()
+      .addVariant()
+        .withName(memberName)
+        .done()
+      .withEmail(email)
+      .withUsername(username)
+      .withPassword(password)
+      .withMemberTypeId(memberTypeId)
+      .build();
+    return await this.create(member);
+  }
+
+  async createMemberWithMemberGroup(memberName: string, memberTypeId: string, email: string, username: string, password: string, memberGroupId: string) {
+    const member = new MemberBuilder()
+      .addVariant()
+        .withName(memberName)
+        .done()
+      .withEmail(email)
+      .withUsername(username)
+      .withPassword(password)
+      .withMemberTypeId(memberTypeId)
+      .addGroup(memberGroupId)
+      .build();
+    return await this.create(member);
+  }
+}
---
+++ lib/helpers/MemberUiHelper.ts
@@ -0,0 +1,124 @@
+import {Page, Locator, expect} from "@playwright/test";
+import {UiBaseLocators} from "./UiBaseLocators";
+
+export class MemberUiHelper extends UiBaseLocators {
+  private readonly membersTab: Locator;
+  private readonly searchTxt: Locator;
+  private readonly memberNameTxt: Locator;
+  private readonly commentsTxt: Locator;
+  private readonly memberTab: Locator;
+  private readonly usernameTxt: Locator;
+  private readonly emailTxt: Locator;
+  private readonly passwordTxt: Locator;
+  private readonly confirmPasswordTxt: Locator;
+  private readonly newPasswordTxt: Locator;
+  private readonly approvedSlider: Locator;
+  private readonly lockedOutSlider: Locator;
+  private readonly twoFactorAuthenticationSlider: Locator;
+  private readonly memberInfoItems: Locator;
+  private readonly changePasswordBtn: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.membersTab = page.locator('uui-tab[label="Members"]');
+    this.searchTxt = page.locator('#input-search');
+    this.memberNameTxt = page.locator('#name-input #input');
+    this.commentsTxt = page.locator('#textarea');
+    this.memberTab = page.getByText('Member', { exact: true })
+    this.usernameTxt = page.locator('[label="Username"] #input');
+    this.emailTxt = page.locator('[label="Email"] #input');
+    this.passwordTxt = page.locator('[label="Password"] #input');
+    this.confirmPasswordTxt = page.locator('[label="Confirm password"] #input');
+    this.newPasswordTxt = page.locator('[label="New password"] #input');
+    this.approvedSlider = page.locator('[label="Approved"] #slider');
+    this.lockedOutSlider = page.locator('[label="Locked out"] #slider');
+    this.twoFactorAuthenticationSlider = page.locator('[label="Two-Factor authentication"] #slider');
+    this.memberInfoItems = page.locator('.general-item');
+    this.changePasswordBtn = page.getByLabel('Change password', {exact: true});
+  }
+
+  async clickMembersTab() {
+    await this.membersTab.click();
+    await this.page.waitForTimeout(200);
+  }
+
+  async clickMemberTab() {
+    await this.memberTab.click();
+    await this.page.waitForTimeout(200);
+  }
+
+  async clickMemberLinkByName(memberName: string) {
+    await this.page.getByRole('link', {name: memberName}).click();
+    await this.page.waitForTimeout(200);
+  }
+
+  async enterSearchKeyword(keyword: string) {
+    await this.searchTxt.clear();
+    await this.searchTxt.fill(keyword);
+  }
+
+  async enterMemberName(name: string) {
+    await this.memberNameTxt.clear();
+    await this.memberNameTxt.fill(name);
+  }
+
+  async enterComments(comment: string) {
+    await this.commentsTxt.clear();
+    await this.commentsTxt.fill(comment);
+  }
+
+  async enterUsername(username: string) {
+    await this.usernameTxt.clear();
+    await this.usernameTxt.fill(username);
+  }
+
+  async enterEmail(email: string) {
+    await this.emailTxt.clear();
+    await this.emailTxt.fill(email);
+  }
+
+  async enterPassword(password: string) {
+    await this.passwordTxt.clear();
+    await this.passwordTxt.fill(password);
+  }
+
+  async enterConfirmPassword(password: string) {
+    await this.confirmPasswordTxt.clear();
+    await this.confirmPasswordTxt.fill(password);
+  }
+
+  async enterNewPassword(password: string) {
+    await this.newPasswordTxt.clear();
+    await this.newPasswordTxt.fill(password);
+  }
+
+  async chooseMemberGroup(memberGroupName: string) {
+    await this.clickChooseButton()
+    await this.page.getByText(memberGroupName, {exact: true}).click();
+    await this.submitBtn.click();
+  }
+
+  async doesMemberInfoHaveValue(infoName: string, value: string) {
+    return expect(this.memberInfoItems.filter({hasText: infoName}).locator('span')).toHaveText(value);
+  }
+
+  async clickApprovedSlider() {
+    await this.approvedSlider.click();
+  }
+
+  async clickLockedOutSlider() {
+    await this.lockedOutSlider.click();
+  }
+
+  async clickTwoFactorAuthenticationSlider() {
+    await this.twoFactorAuthenticationSlider.click();
+  }
+
+  async clickChangePasswordButton() {
+    await this.changePasswordBtn.click();
+  }
+
+  async clickRemoveMemberGroupByName(memberGroupName: string) {
+    await this.page.locator('uui-button[label="Remove ' + memberGroupName + '"]').click();
+  }
+}
