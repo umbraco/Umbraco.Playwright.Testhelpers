2bd8648777d65a16fcd3e4524d1676d35aac32b8
Andreas Zerbst <andr317c@live.dk>
2024-04-19 12:25:00 +0200

    Added comment

--- lib/helpers/testExtension.ts
+++ lib/helpers/testExtension.ts
@@ -4,6 +4,7 @@ import {ApiHelpers, UiHelpers} from ".";
 const test = base.extend<{ umbracoApi: ApiHelpers } & { umbracoUi: UiHelpers }>({
   umbracoApi: async ({page}, use) => {
     const umbracoApi = new ApiHelpers(page);
+    // Runs the isAccessTokenValid before each implementation of umbracoApi in our tests (Which is every single one)
     await umbracoApi.isAccessTokenValid();
     await use(umbracoApi);
   },
