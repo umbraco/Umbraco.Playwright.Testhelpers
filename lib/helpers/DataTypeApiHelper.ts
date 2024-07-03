6e525f8eb7902e557b32bde580f8771fde595580
Nhu Dinh <hnd@umbraco.dk>
2024-07-03 16:58:17 +0700

    Added method to create checkbox list data type

--- lib/helpers/DataTypeApiHelper.ts
+++ lib/helpers/DataTypeApiHelper.ts
@@ -1,5 +1,5 @@
 ﻿import {ApiHelpers} from "./ApiHelpers";
-import {DatePickerDataTypeBuilder} from "@umbraco/json-models-builders";
+import {CheckboxListDataTypeBuilder, DatePickerDataTypeBuilder} from "@umbraco/json-models-builders";
 
 export class DataTypeApiHelper {
   api: ApiHelpers
@@ -207,6 +207,16 @@ export class DataTypeApiHelper {
 
     const dataType = new DatePickerDataTypeBuilder()
       .withName(name)
+      .build();
+    return await this.save(dataType);
+  }
+
+  async createCheckboxListDataType(name: string, options: string[]) {
+    await this.ensureNameNotExists(name);
+
+    const dataType = new CheckboxListDataTypeBuilder()
+      .withName(name)
+      .withItems(options)
       .build();
     return await this.save(dataType);
   }
