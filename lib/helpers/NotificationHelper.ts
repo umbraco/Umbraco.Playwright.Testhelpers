df20e8dfe7c83d6ffbc10dfa95133d3c4221fb88
Nhu Dinh <hnd@umbraco.dk>
2024-09-27 17:28:24 +0700

    Added notification helper

---
+++ lib/helpers/NotificationHelper.ts
@@ -0,0 +1,14 @@
+export class NotificationHelper {
+
+  public static readonly content = {
+    created: "Created",
+    saved: "Saved",
+    published: "Document published",
+    unpublished: "Document unpublished",
+    copied: "Copied",
+    moved: "Moved",
+    movedToRecycleBin: "Trashed",
+    deleted: "Trashed",
+    emptiedRecycleBin: "Recycle Bin Emptied",
+  }
+}
--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,5 +1,6 @@
-export { ApiHelpers } from './ApiHelpers';
-export { UiHelpers } from './UiHelpers';
-export { AliasHelper} from './AliasHelper';
-export { test } from './testExtension';
+export {ApiHelpers} from './ApiHelpers';
+export {UiHelpers} from './UiHelpers';
+export {AliasHelper} from './AliasHelper';
+export {test} from './testExtension';
 export {ConstantHelper} from './ConstantHelper';
+export {NotificationHelper} from './NotificationHelper';
