47578273e1970ffac94e7a486d50358c9c734b4c
Nhu Dinh <hnd@umbraco.dk>
2024-10-07 11:27:48 +0700

    Added more notification message

--- lib/helpers/NotificationHelper.ts
+++ lib/helpers/NotificationHelper.ts
@@ -1,6 +1,6 @@
 export class NotificationHelper {
 
-  public static readonly content = {
+  public static readonly success = {
     created: "Created",
     saved: "Saved",
     published: "Document published",
@@ -8,7 +8,21 @@ export class NotificationHelper {
     copied: "Copied",
     moved: "Moved",
     movedToRecycleBin: "Trashed",
-    deleted: "Trashed",
+    deleted: "Deleted",
     emptiedRecycleBin: "Recycle Bin Emptied",
+    restored: "Restored",
+    duplicated: "Duplicated",
+    folderCreated: "Folder created",
+    folderUpdated: "Folder updated",
+    folderDeleted: "Folder deleted"
+  }
+
+  public static readonly error = {
+    emptyName: "Name was empty or null",
+    duplicateName: "Duplicate name",
+    invalidEmail: "Invalid email supplied",
+    notEmptyFolder: "The folder is not empty",
+    duplicateISOcode: "Duplicate ISO code",
+    notEmpty: 'Not empty'
   }
 }
