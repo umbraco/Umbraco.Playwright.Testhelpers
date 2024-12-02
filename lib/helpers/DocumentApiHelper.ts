2abe36ad71870f7128a45f4973ca71b748f9d41e
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2024-12-02 19:41:33 +0700

    Added test helper for rendering content with multi media picker (#178)
    * Added api helper to create published document with two media picker
    
    * Added api helper to create template with displaying multiple media picker value
    
    * Bumped version

--- lib/helpers/DocumentApiHelper.ts
+++ lib/helpers/DocumentApiHelper.ts
@@ -576,4 +576,34 @@ export class DocumentApiHelper {
     await this.publish(documentId);
     return {'documentId': documentId, 'temporaryFileId': temporaryFile.temporaryFileId};
   }
+
+  async createPublishedDocumentWithTwoMediaPicker(documentName: string, firstMediaPickerId: string, secondMediaPickerId: string, dataTypeId: string, templateId: string, propertyName: string = 'Test Property Name', documentTypeName: string = 'Test Document Type') {
+    // Create document type
+    let documentTypeId = await this.api.documentType.createDocumentTypeWithPropertyEditorAndAllowedTemplate(documentTypeName, dataTypeId, propertyName, templateId);
+    documentTypeId = documentTypeId === undefined ? '' : documentTypeId;
+    await this.ensureNameNotExists(documentName);
+
+    const document = new DocumentBuilder()
+      .withDocumentTypeId(documentTypeId)
+      .addVariant()
+        .withName(documentName)
+        .done()
+      .addValue()
+        .withAlias(AliasHelper.toAlias(propertyName))
+        .addMediaPickerValue()
+          .withMediaKey(firstMediaPickerId)
+          .done()
+        .addMediaPickerValue()
+          .withMediaKey(secondMediaPickerId)
+          .done()
+        .done()
+      .build();
+
+    // Create document
+    let documentId = await this.create(document);
+    documentId = documentId === undefined ? '' : documentId;
+    // Publish document
+    await this.publish(documentId);
+    return documentId;
+  }
 }
