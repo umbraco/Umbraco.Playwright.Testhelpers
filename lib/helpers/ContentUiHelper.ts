2172ee1fb08449506908b47cfeaafe67ffe1a1ad
Nhu Dinh <hnd@umbraco.dk>
2025-01-13 14:08:23 +0700

    Updated ui helper for enter link picker form by using press spacebar

--- lib/helpers/ContentUiHelper.ts
+++ lib/helpers/ContentUiHelper.ts
@@ -562,19 +562,31 @@ export class ContentUiHelper extends UiBaseLocators {
     await this.sidebarModal.getByText(linkName, {exact: true}).click();
   }
 
-  async enterLink(value: string) {
+  async enterLink(value: string, isPress: boolean = false) {
     await this.linkTxt.clear();
-    await this.linkTxt.fill(value);
+    if (isPress) {
+      await this.linkTxt.press(value);
+    } else {
+      await this.linkTxt.fill(value);
+    }   
   }
 
-  async enterAnchorOrQuerystring(value: string) {
+  async enterAnchorOrQuerystring(value: string, isPress: boolean = false) {
     await this.anchorQuerystringTxt.clear();
-    await this.anchorQuerystringTxt.fill(value);
+    if (isPress) {
+      await this.anchorQuerystringTxt.press(value);
+    } else {
+      await this.anchorQuerystringTxt.fill(value);
+    }   
   }
 
-  async enterLinkTitle(value: string) {
+  async enterLinkTitle(value: string, isPress: boolean = false) {
     await this.linkTitleTxt.clear();
-    await this.linkTitleTxt.fill(value);
+    if (isPress) {
+      await this.linkTitleTxt.press(value);
+    } else {
+      await this.linkTitleTxt.fill(value);
+    }   
   }
 
   async removeUrlPickerByName(linkName: string) {
@@ -956,6 +968,6 @@ export class ContentUiHelper extends UiBaseLocators {
   }
 
   async clickTipTapToolbarIconWithTitle(iconTitle: string) {
-    await this.tipTapPropertyEditor.getByTitle(iconTitle).locator('svg').click();
+    await this.tipTapPropertyEditor.getByTitle(iconTitle, {exact: true}).locator('svg').click();
   }
 }
