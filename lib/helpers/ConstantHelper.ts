d3a3f68775d028800d5ff4549ef0215f4990f41b
Andreas Zerbst <andr317c@live.dk>
2025-02-11 13:47:15 +0100

    Added constants

--- lib/helpers/ConstantHelper.ts
+++ lib/helpers/ConstantHelper.ts
@@ -18,10 +18,20 @@
 
   public static readonly validationMessages = {
     emptyLinkPicker: 'Please enter an anchor or querystring, or select a published document or media item, or manually configure the URL',
+    invalidValue: 'Value is invalid, it does not match the correct pattern'
   }
 
   public static readonly inputTypes = {
     general: 'input',
     tipTap: 'umb-input-tiptap'
+  }
+  
+  public static readonly notificationMessages = {
+    success: 'Success',
+    error: 'Error',
+    saved: 'Saved',
+    published: 'Published',
+    unpublished: 'Unpublished',
+    documentWasNotPublished: 'Document was not published, but we saved it for you.'
   }
 }
