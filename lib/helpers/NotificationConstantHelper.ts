5c31499fa24a9939de51ba35478664181f90b3c5
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2025-02-12 10:18:57 +0100

    V15 Added complex block test helpers (#196)
    * Added method for creating elementType with Regex
    
    * Added dataType helpers
    
    * Added helpers
    
    * Added constants
    
    * Bumped version
    
    * Moved constants

--- lib/helpers/NotificationConstantHelper.ts
+++ lib/helpers/NotificationConstantHelper.ts
@@ -2,6 +2,7 @@ export class NotificationConstantHelper {
 
   public static readonly success = {
     created: "Created",
+    success: 'Success',
     saved: "Saved",
     published: "Document published",
     unpublished: "Document unpublished",
@@ -28,12 +29,14 @@ export class NotificationConstantHelper {
   }
 
   public static readonly error = {
+    error: 'Error',
     emptyName: "Name was empty or null",
     duplicateName: "Duplicate name",
     invalidEmail: "Invalid email supplied",
     notEmptyFolder: "The folder is not empty",
     duplicateISOcode: "Duplicate ISO code",
     notEmpty: "Not empty",
-    noAccessToResource: "The authenticated user do not have access to this resource"
+    noAccessToResource: "The authenticated user do not have access to this resource",
+    documentWasNotPublished: 'Document was not published, but we saved it for you.'
   }
 }
