d01e0523463cc7d32078a770dccce1a3143af6da
Andreas Zerbst <andr317c@live.dk>
2025-05-05 14:01:18 +0200

    Added helper

--- lib/helpers/DataTypeApiHelper.ts
+++ lib/helpers/DataTypeApiHelper.ts
@@ -1506,6 +1506,16 @@ export class DataTypeApiHelper {
     const layoutStylesheetValue = blockEditor.values.find(value => value.alias === 'layoutStylesheet');
     return layoutStylesheetValue?.value === '/wwwroot/css/' + stylesheetName;
   }
+
+  async createEmptyRichTextEditor(richTextEditorName: string) {
+    await this.ensureNameNotExists(richTextEditorName);
+
+    const richTextEditor = new TiptapDataTypeBuilder()
+      .withName(richTextEditorName)
+      .build();
+
+    return await this.save(richTextEditor);
+  }
   
   async createRichTextEditorWithABlock(richTextEditorName: string, contentElementTypeId: string) {
     await this.ensureNameNotExists(richTextEditorName);
