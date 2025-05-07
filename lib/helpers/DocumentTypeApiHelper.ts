754aa2c542cde4363c804fc6ecef175aa8104c76
Nhu Dinh <hnd@umbraco.dk>
2025-05-07 12:12:23 +0700

    Added api helper to create variant document type with allowed child node and invariant property editor

--- lib/helpers/DocumentTypeApiHelper.ts
+++ lib/helpers/DocumentTypeApiHelper.ts
@@ -766,6 +766,37 @@ export class DocumentTypeApiHelper {
         .done()
       .withVariesByCulture(true)
       .build();
+
+    return await this.create(documentType);
+  }
+
+  async createVariantDocumentTypeWithAllowedChildNodeAndInvariantPropertyEditor(documentTypeName: string, allowedChildNodeId: string, dataTypeName: string, dataTypeId: string, groupName: string = "TestGroup") {
+    const crypto = require('crypto');
+    const containerId = crypto.randomUUID();
+    await this.ensureNameNotExists(documentTypeName);
+
+    const documentType = new DocumentTypeBuilder()
+      .withName(documentTypeName)
+      .withAlias(AliasHelper.toAlias(documentTypeName))
+      .withAllowedAsRoot(true)
+      .addContainer()
+        .withName(groupName)
+        .withId(containerId)
+        .withType("Group")
+        .done()
+      .addProperty()
+        .withContainerId(containerId)
+        .withAlias(AliasHelper.toAlias(dataTypeName))
+        .withName(dataTypeName)
+        .withDataTypeId(dataTypeId)
+        .withVariesByCulture(false)
+        .done()
+      .addAllowedDocumentType()
+        .withId(allowedChildNodeId)
+        .done()
+      .withVariesByCulture(true)
+      .build();
+      
     return await this.create(documentType);
   }
 }
