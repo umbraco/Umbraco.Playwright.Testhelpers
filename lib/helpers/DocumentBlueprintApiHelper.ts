40c93a5f93fb572eea4f2e70e981bb7788db1d34
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2025-07-11 13:21:20 +0700

    V16 Updated test helper for document blueprint (#268)
    * Updated api helper for document due to json builder changes
    
    * Ensure the language is not exist before creating
    
    * Added api helper to create document blueprint
    
    * Bumped version
    
    * Fixed comments

--- lib/helpers/DocumentBlueprintApiHelper.ts
+++ lib/helpers/DocumentBlueprintApiHelper.ts
@@ -1,5 +1,5 @@
 ﻿import {ApiHelpers} from "./ApiHelpers";
-import {DocumentBlueprintsBuilder} from "@umbraco/json-models-builders";
+import {AliasHelper, DocumentBlueprintsBuilder} from "@umbraco/json-models-builders";
 
 export class DocumentBlueprintApiHelper {
   api: ApiHelpers;
@@ -124,12 +124,143 @@ export class DocumentBlueprintApiHelper {
 
   async createDefaultDocumentBlueprint(documentBlueprintName: string, documentTypeId: string) {
     await this.ensureNameNotExists(documentBlueprintName);
-    const document = new DocumentBlueprintsBuilder()
+    const documentBlueprint = new DocumentBlueprintsBuilder()
       .withDocumentTypeId(documentTypeId)
       .addVariant()
         .withName(documentBlueprintName)
         .done()
       .build();
-    return await this.create(document);
+    return await this.create(documentBlueprint);
+  }
+
+  async createFromDocument(documentBlueprintName: string, documentId: string, parentId?: string) {
+    const documentBlueprintData = {
+      "name": documentBlueprintName,
+      "parent": parentId ? parentId : null,
+      "document": {
+        "id": documentId
+      }
+    };
+    const response = await this.api.post(this.api.baseUrl + '/umbraco/management/api/v1/document-blueprint/from-document', documentBlueprintData);
+    return response.headers().location.split("v1/document-blueprint/").pop();
+  }
+
+  async createDocumentBlueprintWithTextBoxValue(documentBlueprintName: string, documentTypeId: string, dataTypeName: string, text: string) {
+    await this.ensureNameNotExists(documentBlueprintName);
+
+    const documentBlueprint = new DocumentBlueprintsBuilder()
+      .withDocumentTypeId(documentTypeId)
+      .addVariant()
+        .withName(documentBlueprintName)
+        .done()
+      .addValue()
+        .withAlias(AliasHelper.toAlias(dataTypeName))
+        .withValue(text)
+        .withEditorAlias('Umbraco.TextBox')
+        .done()
+      .build();
+
+    return await this.create(documentBlueprint);
+  }
+
+  async createDefaultDocumentBlueprintWithABlockListEditorAndBlockWithValue(documentBlueprintName: string, documentTypeName: string, blockListDataTypeName: string, elementTypeId: string, elementTypePropertyAlias: string, elementTypePropertyEditorAlias: string, elementTypePropertyValue: string, groupName: string) {
+    const crypto = require('crypto');
+    const blockContentKey = crypto.randomUUID();
+    const blockListDataTypeId = await this.api.dataType.createBlockListDataTypeWithABlock(blockListDataTypeName, elementTypeId) || '';
+    const documentTypeId = await this.api.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListDataTypeName, blockListDataTypeId, groupName) || '';
+    await this.ensureNameNotExists(documentBlueprintName);
+
+    const documentBlueprint = new DocumentBlueprintsBuilder()
+      .withDocumentTypeId(documentTypeId)
+      .addVariant()
+        .withName(documentBlueprintName)
+        .done()
+      .addValue()
+        .withAlias(AliasHelper.toAlias(blockListDataTypeName))
+        .withEditorAlias('Umbraco.BlockList')
+        .addBlockListValue()
+          .addContentData()
+            .withContentTypeKey(elementTypeId)
+            .withKey(blockContentKey)
+            .addContentDataValue()
+              .withAlias(elementTypePropertyAlias)
+              .withEditorAlias(elementTypePropertyEditorAlias)
+              .withValue(elementTypePropertyValue)
+              .done()
+            .done()
+          .addExpose()
+            .withContentKey(blockContentKey)
+            .done()
+          .addLayout()
+            .withContentKey(blockContentKey)
+            .done()
+          .done()
+        .done()
+      .build();
+    
+    return await this.create(documentBlueprint);
+  }
+
+  async createDefaultDocumentBlueprintWithABlockGridEditorAndBlockWithValue(documentBlueprintName: string, documentTypeName: string, blockGridDataTypeName: string, elementTypeId: string, elementTypePropertyAlias: string, elementTypePropertyEditorAlias: string, elementTypePropertyValue: string, groupName: string = 'TestGroup') {
+    const crypto = require('crypto');
+    const blockContentKey = crypto.randomUUID();
+    const blockGridDataTypeId = await this.api.dataType.createBlockGridWithABlockAndAllowAtRoot(blockGridDataTypeName, elementTypeId, true) || '';
+    const documentTypeId = await this.api.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockGridDataTypeName, blockGridDataTypeId, groupName) || '';
+    await this.ensureNameNotExists(documentBlueprintName);
+
+    const documentBlueprint = new DocumentBlueprintsBuilder()
+      .withDocumentTypeId(documentTypeId)
+      .addVariant()
+        .withName(documentBlueprintName)
+        .done()
+      .addValue()
+        .withAlias(AliasHelper.toAlias(blockGridDataTypeName))
+        .withEditorAlias('Umbraco.BlockGrid')
+        .addBlockGridValue()
+          .addContentData()
+            .withContentTypeKey(elementTypeId)
+            .withKey(blockContentKey)
+            .addContentDataValue()
+              .withAlias(elementTypePropertyAlias)
+              .withEditorAlias(elementTypePropertyEditorAlias)
+              .withValue(elementTypePropertyValue)
+              .done()
+            .done()
+          .addExpose()
+            .withContentKey(blockContentKey)
+            .done()
+          .addLayout()
+            .withContentKey(blockContentKey)
+            .done()
+          .done()
+        .done()
+      .build();
+
+    return await this.create(documentBlueprint);
+  }
+
+  async createDocumenBlueprintWithEnglishCultureAndDanishCultureAndTextBoxValue(documentBlueprintEnglishName: string, documentBlueprintDanishName: string, documentTypeId: string, dataTypeName: string, textContent: string, varyByCultureForText: boolean = false) {
+    await this.ensureNameNotExists(documentBlueprintEnglishName);
+    const cultureValue = varyByCultureForText ? 'en-US' : null;
+
+    const documentBlueprint = new DocumentBlueprintsBuilder()
+      .withDocumentTypeId(documentTypeId)
+      .addVariant()
+        .withName(documentBlueprintEnglishName)
+        .withCulture('en-US')
+        .done()
+      .addVariant()
+        .withName(documentBlueprintDanishName)
+        .withCulture('da')
+        .done()
+      .addValue()
+        .withAlias(AliasHelper.toAlias(dataTypeName))
+        .withValue(textContent)
+        .withCulture(cultureValue)
+        .withEditorAlias('Umbraco.TextBox')
+        .done()
+      .build();
+
+    return await this.create(documentBlueprint);
   }
 } 
