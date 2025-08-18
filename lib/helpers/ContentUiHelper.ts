69d212b9afaf1abb845dd8a4c93b8cabcf690201
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2025-08-18 14:57:09 +0700

    V16 Fixed the remove file button locator (#280)
    * Updated locator for remove file button
    
    * Bumped version

--- lib/helpers/ContentUiHelper.ts
+++ lib/helpers/ContentUiHelper.ts
@@ -201,7 +201,7 @@ export class ContentUiHelper extends UiBaseLocators {
     this.anchorQuerystringTxt = page.getByLabel('#value or ?key=value');
     this.linkTitleTxt = this.linkPickerModal.getByLabel('Title');
     this.tagItems = page.locator('uui-tag');
-    this.removeFilesBtn = page.locator('umb-input-upload-field [label="Remove file(s)"]');
+    this.removeFilesBtn = page.locator('umb-input-upload-field [label="Clear file(s)"]');
     this.toggleBtn = page.locator('umb-property-editor-ui-toggle #toggle');
     this.toggleInput = page.locator('umb-property-editor-ui-toggle span');
     this.documentTypeWorkspace = this.sidebarModal.locator('umb-document-type-workspace-editor');
