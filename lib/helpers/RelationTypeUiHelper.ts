6c748da84b736a0ce94fcd34a8be54d90eeb0df3
Nhu Dinh <hnd@umbraco.dk>
2025-10-21 22:30:03 +0700

    Format code

--- lib/helpers/RelationTypeUiHelper.ts
+++ lib/helpers/RelationTypeUiHelper.ts
@@ -34,7 +34,7 @@ export class RelationTypeUiHelper extends UiBaseLocators{
   }
 
   async clickActionsMenuForRelationType(name: string) {
-    await this.clickActionsMenuForName(name); 
+    await this.clickActionsMenuForName(name);
   }
   
   async goToRelationTypeWithName(name: string) {
